da9b6fce800216903b1c2b1c9564f7fbc6d0744d
Dan Rinehimer <rinehimer@gmail.com>
2010-06-30 16:00:30 +0000

    Added some simple tests.

---
+++ test/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for dir in `find . -mindepth 1 -type d` ; do
+    $jxtl -s -x t.xml -t $dir/input > $dir/test.output
+
+    if [ $? -ne 0 ] ; then
+        echo "jxtl exited with non-zero status when running test in $dir"
+        exit 1
+    fi
+
+    diff $dir/output $dir/test.output > /dev/null 2>&1
+    if [ $? -ne 0 ] ; then
+        echo "Failed test in $dir"
+        exit 1
+    fi
+    rm $dir/test.output
+done
+
+exit 0
