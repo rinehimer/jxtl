742481d70ce5ac4b437a8ada92810f5a9d0c86f1
Dan Rinehimer <rinehimer@gmail.com>
2010-09-06 12:16:44 +0000

    Fixed a couple bugs with xml2json.c.  It wasn't correctly checking the number of arguments passed to it and would try to convert even if no filename was passed.

--- src/xml2json.c
+++ src/xml2json.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <apr_pools.h>
 
 #include "json.h"
@@ -7,26 +8,26 @@
 
 void xml2json_usage( const char *prog_name )
 {
-  printf( "Usage: %s <file>\n", prog_name );
-  printf( "<file> can be \"-\" to signify stdin\n" );
+  fprintf( stderr, "Usage: %s <file>\n", prog_name );
+  fprintf( stderr, "<file> can be \"-\" to signify stdin\n" );
+  exit( 1 );
 }
 
 int main( int argc, char **argv )
 {
   json_writer_t *writer;
-  int ret = 0;
+  int ret;
   apr_pool_t *mp;
 
   apr_app_initialize( NULL, NULL, NULL );
   apr_pool_create( &mp, NULL );
   writer = json_writer_create( mp );
 
-  if ( argc < 1 ) {
+  if ( argc < 2 ) {
     xml2json_usage( argv[0] );
-    ret = 1;
   }
   
-  if ( ( ret == 0 ) && ( xml_file_to_json( argv[1], writer, 1 ) == 0 ) ) {
+  if ( xml_file_to_json( argv[1], writer, 1 ) == 0 ) {
     json_object_print( writer->json, 1 );
     ret = 0;
   }
