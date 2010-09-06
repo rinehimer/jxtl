a015100327d8e23a6c5a18b4f895ee63c87d67c9
Dan Rinehimer <rinehimer@gmail.com>
2010-09-06 12:11:19 +0000

    All of the tests are now also run with equivalent JSON to verify that they both yield the same output.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+check_status() {
+    if [ $? -ne 0 ] ; then
+        echo $1
+        exit 1
+    fi
+}
+
+run_test() {
+    local dir=$1
+    local args=$2
+    $jxtl $args -t $dir/input > $dir/test.output
+    check_status "jxtl with args $args had bad exit status in $dir"
+    diff $dir/output $dir/test.output > /dev/null 2>&1
+    check_status "Failed test in $dir"
+    rm $dir/test.output
+}
+
+rm -f t.json
+$xml2json t.xml > t.json
+check_status "failed to convert test XML to JSON"
+
 for dir in `find . -mindepth 1 -type d` ; do
     if [ -f $dir/input ] ; then
-        $jxtl -s -x t.xml -t $dir/input > $dir/test.output
-        
-        if [ $? -ne 0 ] ; then
-            echo "jxtl exited with non-zero status when running test in $dir"
-            exit 1
-        fi
-        
-        diff $dir/output $dir/test.output > /dev/null 2>&1
-        if [ $? -ne 0 ] ; then
-            echo "Failed test in $dir"
-            exit 1
-        fi
-        rm $dir/test.output
+        run_test $dir "-s -x t.xml"
+        run_test $dir "-j t.json"
     fi
 done
 
